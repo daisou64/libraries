694b96a3cfd06a61a1f6f12a4e77677c34c03eb6
daisou64win11 <sample@example.ccc>
2025-01-04 23:23:08 +0900

    refactoring

--- VB/Graph/GraphLogic2.vb
+++ VB/Graph/GraphLogic2.vb
@@ -3,36 +3,44 @@
 Public Class GraphLogic2
 
     Public Shared Function AdjustRangeWithMargin(list As List(Of Decimal)) As (Decimal, Decimal, Decimal)
+        Const ScaleLines As Integer = 4
 
+        '0.0009 → 4, 0.0023 → 3
+        Dim floatValueDigit As Func(Of Decimal, Double) =
+            Function(rng) rng.ToString() _
+                                .SkipWhile(Function(x) x <> ".") _
+                                .Skip(1) _
+                                .TakeWhile(Function(x) x = "0") _
+                                .ToList() _
+                                .Count + 1
+        Dim floatValueRoundUnit As Func(Of Decimal, Double) = Function(rng) Math.Pow(10, -floatValueDigit(rng))
+        Const IntValueRoundUnit As Double = 5
+
+        Dim margin As Func(Of Decimal, Double) = Function(rng) rng * 0.05
+        Dim roundMax As Func(Of Decimal, Double, Double) = Function(val, unit) Math.Ceiling(val / unit) * unit
+        Dim roundMin As Func(Of Decimal, Double, Double) = Function(val, unit) Math.Floor(val / unit) * unit
 
         Dim maxValue = list.Max()
         Dim minValue = list.Min()
 
+        '初期値の範囲
         Dim range = maxValue - minValue
 
-        Dim margin = range * 0.05
+        '初期値の範囲によって丸め単位を決定する
+        Dim roundUnit As Double = If(range < 1, floatValueRoundUnit(range), IntValueRoundUnit)
 
-        Dim addMarginMax = maxValue + margin
-        Dim addMarginMin = minValue - margin
+        '初期値の範囲による丸め処理の実施
+        Dim adjustMax = roundMax(maxValue + margin(range), roundUnit)
+        Dim adjustMin = roundMin(minValue - margin(range), roundUnit)
 
-        Dim floatValueDigit As Func(Of Decimal, Double) = Function(rng) rng.ToString().SkipWhile(Function(x) x <> ".").Skip(1).TakeWhile(Function(x) x = "0").ToList().Count
-        Dim floatValueRoundUnit = Math.Pow(10, -floatValueDigit(range) - 1)
-        Const intValueRoundUnit = 5
+        '調整値の範囲
+        Dim adjustRange = adjustMax - adjustMin
 
-        Dim roundUnit As Double = If(range < 1, floatValueRoundUnit, intValueRoundUnit)
+        '目盛値の決定
+        Dim interval = Math.Ceiling(adjustRange / ScaleLines / roundUnit) * roundUnit
 
-        Dim adjustMax = Math.Ceiling(addMarginMax / roundUnit) * roundUnit
-        Dim adjustMin = Math.Floor(addMarginMin / roundUnit) * roundUnit
-
-        Dim totalRange = adjustMax - adjustMin
-
-        Dim interval = Math.Ceiling(totalRange / 4 / roundUnit) * roundUnit
-
-        Dim adjustMax2 = Math.Ceiling(adjustMax / interval) * interval
-        Dim adjustMin2 = Math.Floor(adjustMin / interval) * interval
-
-
-        Return (adjustMax2, adjustMin2, interval)
+        '最大値・最小値を目盛値上の値になるよう丸める
+        Return (roundMax(adjustMax, interval), roundMin(adjustMin, interval), interval)
     End Function
 
 End Class
